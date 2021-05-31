60803e3ec7b3117606e6a4ee3ecb1c5c4e3f33f1
leobar37_2 <leonbarbozaelmerjoselito@gmail.com>
2021-05-30 21:29:55 -0500

    my first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
